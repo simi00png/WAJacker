7cdf58a749e2c99d1046d3870e61491546d971aa
simi00png <simimd7773@gmail.com>
2025-06-17 16:15:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "f0JzUQqC#mTTcf_mUQA6atTxvB7p3tyh2-07v3NoK40rDu6h3ids",// session id 💙
+HACKER : process.env.HACKER || "94760224138" //Addmin number 💙
 };
